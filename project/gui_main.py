14d07908b9e4706915399d5ac727330ad52d131e
watersheep32 <75270450+watersheep32@users.noreply.github.com>
2026-07-10 16:50:50 +0100

    improvements in ease of usage and intuitivity

--- project/gui_main.py
+++ project/gui_main.py
@@ -3,26 +3,29 @@ from tkinter import font
 from tkinter import ttk
 import project
 
-def add_entry(window):
+def add_entry(window, add_button):
     name = Entry(window, justify= "center", font=(fonter, 9), fg=colours[2], bg=colours[3], relief="sunken")
-    cost = Entry(window, justify= "center", font=(fonter, 9), fg=colours[2], bg=colours[3], relief="sunken")
     amount = Entry(window, justify= "center", font=(fonter, 9), fg=colours[2], bg=colours[3], relief="sunken")
-    name_label = Label(window, text="")
-    name.grid(column=1, pady=10, columnspan=2)
-    cost.grid(column=1, pady=10, columnspan=2)
-    amount.grid(column=1, pady=10, columnspan=2)
-    submitter = Button(window, text="submit", font=(fonter, 9), fg=colours[1], bg=colours[2], command=lambda: submit_entry(window, tree, submitter, name, cost, amount))
-    submitter.grid(column=2, padx=20)
+    name_label = Label(window, text="enter name of transaction:", fg=colours[1], bg=colours[2], font=(fonter, 11))
+    amount_label = Label(window, text="enter amount of transaction (+ for earning and - for expense):", fg=colours[1], bg=colours[2], font=(fonter, 11))
+    name_label.grid(column=1, pady=10, columnspan=2)
+    name.grid(column=1, columnspan=2)
+    amount_label.grid(column=1, pady=10, columnspan=2)
+    amount.grid(column=1, columnspan=2)
+    submitter = Button(window, text="submit", font=(fonter, 9), fg=colours[1], bg=colours[2], command=lambda: submit_entry(window, tree, submitter, name, amount, name_label, amount_label, add_button))
+    submitter.grid(column=1, columnspan=2)
+    add_button.config(state=DISABLED)
 
-def submit_entry(window, tree, submitter, name, cost, amount):
+def submit_entry(window, tree, submitter, name, amount, name_label, amount_label, add_button):
     name_value = name.get()
     name.destroy()
-    cost_value = cost.get()
-    cost.destroy()
     amount_value = amount.get()
     amount.destroy()
     submitter.destroy()
-    tree.insert('', 'end', values=(name_value, cost_value, amount_value))
+    name_label.destroy()
+    amount_label.destroy()
+    tree.insert('', 'end', values=(name_value, amount_value))
+    add_button.config(state=NORMAL)
 
 colours = ["#646669", "#d1d0c5", "#323437", "#e2b714"]
 fonter = "Roboto Mono"
@@ -43,19 +46,17 @@ stile = ttk.Style()
 stile.theme_use('clam')
 stile.configure('Treeview.Heading', background=colours[3], foreground=colours[2], font=(fonter, 12))
 stile.configure('Treeview', background=colours[0], foreground=colours[1], font=(fonter, 12), fieldbackground=colours[0])
-tree = ttk.Treeview(window, columns=('name', 'expense or earning', 'amount'), show='headings')
+tree = ttk.Treeview(window, columns=('name', 'amount'), show='headings')
 tree.heading("name", text="name")
-tree.heading("expense or earning", text="expense or earning")
 tree.heading("amount", text="amount")
 
 tree.column("#0", width=0, minwidth=0)
 tree.column("name", anchor=CENTER, width=int(width/6))
-tree.column("expense or earning", anchor=CENTER, width=int(width/6))
 tree.column("amount", anchor=CENTER, width=int(width/6))
 
 tree.grid(column=1, columnspan=2, pady=25)
 
-add_button = Button(window, text="add table entry", font=(fonter, 13), fg=colours[1], bg=colours[2], command=lambda: add_entry(window)) #add the function later on
+add_button = Button(window, text="add table entry", font=(fonter, 13), fg=colours[1], bg=colours[2], command=lambda: add_entry(window, add_button)) #add the function later on
 add_button.grid(column=1, columnspan=2)
 
 window.title("h")
