ff0c77931bf2f7efc1ed2d0429c33f1d290b5424
watersheep32 <75270450+watersheep32@users.noreply.github.com>
2026-07-10 15:17:51 +0100

    making more adjustments

--- project/gui_main.py
+++ project/gui_main.py
@@ -7,15 +7,22 @@ def add_entry(window):
     name = Entry(window, justify= "center", font=(fonter, 9), fg=colours[2], bg=colours[3], relief="sunken")
     cost = Entry(window, justify= "center", font=(fonter, 9), fg=colours[2], bg=colours[3], relief="sunken")
     amount = Entry(window, justify= "center", font=(fonter, 9), fg=colours[2], bg=colours[3], relief="sunken")
-    name.grid(column=1, sticky="e", pady=20)
-    submitter = Button(window, text="submit", font=(fonter, 9), fg=colours[1], bg=colours[2], command=lambda: submit_entry(window, tree, submitter, ))
-    submitter.grid(column=2, sticky="e", padx=20)
+    name_label = Label(window, text="")
+    name.grid(column=1, pady=10, columnspan=2)
+    cost.grid(column=1, pady=10, columnspan=2)
+    amount.grid(column=1, pady=10, columnspan=2)
+    submitter = Button(window, text="submit", font=(fonter, 9), fg=colours[1], bg=colours[2], command=lambda: submit_entry(window, tree, submitter, name, cost, amount))
+    submitter.grid(column=2, padx=20)
 
-def submit_entry(window, entry, tree, submitter):
-    entry_value = entry.get()
-    entry.destroy()
+def submit_entry(window, tree, submitter, name, cost, amount):
+    name_value = name.get()
+    name.destroy()
+    cost_value = cost.get()
+    cost.destroy()
+    amount_value = amount.get()
+    amount.destroy()
     submitter.destroy()
-    tree.insert('', 'end', values=(entry_value, "expense", 0))
+    tree.insert('', 'end', values=(name_value, cost_value, amount_value))
 
 colours = ["#646669", "#d1d0c5", "#323437", "#e2b714"]
 fonter = "Roboto Mono"
@@ -48,8 +55,8 @@ tree.column("amount", anchor=CENTER, width=int(width/6))
 
 tree.grid(column=1, columnspan=2, pady=25)
 
-addButton = Button(window, text="add table entry", font=(fonter, 13), fg=colours[1], bg=colours[2], command=lambda: add_entry(window)) #add the function later on
-addButton.grid(column=1, sticky="e")
+add_button = Button(window, text="add table entry", font=(fonter, 13), fg=colours[1], bg=colours[2], command=lambda: add_entry(window)) #add the function later on
+add_button.grid(column=1, columnspan=2)
 
 window.title("h")
 
