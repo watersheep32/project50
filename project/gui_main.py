d08875a4b4774c7b7ba5d67d9f3b89fc0f0c0408
watersheep32 <75270450+watersheep32@users.noreply.github.com>
2026-07-11 15:44:22 +0100

    more erognomic stuff

--- project/gui_main.py
+++ project/gui_main.py
@@ -14,21 +14,20 @@ def add_entry(window):
     name.grid(column=1, columnspan=2)
     amount_label.grid(column=1, pady=10, columnspan=2)
     amount.grid(column=1, columnspan=2)
-    submitter = Button(window, text="submit", font=(fonter, 9), fg=colours[1], bg=colours[2], command=lambda: submit_entry(window, tree, name, submitter, amount, name_label, amount_label, add_button))
-    submitter.grid(column=1, columnspan=2)
     return_label.grid(column=1, columnspan=2)
+    window.bind('<Return>', lambda event: submit_entry(window, tree, name, amount, name_label, amount_label, return_label), add="+")
+    name.focus_set()
 
 
-def submit_entry(window, tree, name, submitter, amount, name_label, amount_label, add_button):
+def submit_entry(window, tree, name, amount, name_label, amount_label, return_label):
     name_value = name.get()
     name.destroy()
     amount_value = amount.get()
     amount.destroy()
     name_label.destroy()
     amount_label.destroy()
-    submitter.destroy()
     tree.insert('', 'end', values=(name_value, amount_value))
-    add_button.config(state=NORMAL)
+    return_label.destroy()
 
 colours = ["#646669", "#d1d0c5", "#323437", "#e2b714"]
 fonter = "Roboto Mono"
@@ -59,7 +58,9 @@ tree.column("amount", anchor=CENTER, width=int(width/6))
 
 tree.grid(column=1, columnspan=2, pady=25)
 
-window.bind('<Return>', lambda event: add_entry(window))
+input_label = Label(window, text="press tab to add a new transaction", fg=colours[1], bg=colours[2], font=(fonter, 13, "bold"))
+input_label.grid(column=1, columnspan=2, pady=10)
+window.bind('<KeyPress-Tab>', lambda event: add_entry(window))
 window.title("h")
 
 window.mainloop()
